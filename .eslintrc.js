0dcf1e35f82d090ed8be337095f8838b3537150c
Pawel Hryniszak <phryniszak@gmail.com>
2021-01-25 21:28:03 +0000

    rtt index.css missing

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+    // http://eslint.org/docs/rules/
+    "env": {
+        "node": true,
+        "browser": true,
+        "es6": true
+    },
+    "parserOptions": {
+        "ecmaVersion": 2019,
+        "sourceType": "module"
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            "error",
+            4,
+            { "SwitchCase": 1 }
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-console": 0
+    },
+    "globals": {
+        "debug": "writable",
+        "Terminal": "readonly",
+        "FitAddon": "readonly",
+        "mcu": "readonly"
+    }
+};
